fa2383cc1538016f959d93e467f694a6a445fc52
chabuuuu <87564111+chabuuuu@users.noreply.github.com>
2023-12-29 02:50:17 +0700

    switch to 443 due to https

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 //const port = process.env.PORT || 3000;
-const port = 80;
+const port = 443;
 const route = require('./routes')
 const morgan = require('morgan');
 
