6c303e74efee6401c4120c072b77fc6e42664157
chabuuuu <87564111+chabuuuu@users.noreply.github.com>
2023-12-29 03:03:04 +0700

    change to http

--- src/app/controllers/ItemsController.js
+++ src/app/controllers/ItemsController.js
@@ -3,7 +3,7 @@ const host = "3.25.245.200"
 class ItemsController {
     //[Get] /items/:slug
     async store(req, res, next) {
-        return res.json({status: "success", url: `${host}/photo/item/${req.imagename}`})
+        return res.json({status: "success", url: `http://${host}/photo/item/${req.imagename}`})
     }
 
     //[Get] /items/create
